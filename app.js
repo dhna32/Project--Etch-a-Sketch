03d21a291ee2379ba2b7939bb1f159161ac1702d
Diegochits <54562062+dhna32@users.noreply.github.com>
2020-04-08 16:02:18 -0500

    Grid in function

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const gridContainer = document.querySelector('#gridContainer');
+const gridSize = 720;
+const array = 64;
+
+const canvasGrid = gridSize/array;
+console.log(canvasGrid);
+
+for (let i = 0; i < array; i++) {
+    const row = document.createElement('div');
+    row.classList.add = 'grid';
+    row.className = 'grid';
+    for (let j = 0; j < array; j++) {
+        const grid = document.createElement('div');
+        grid.style.height = `${canvasGrid-2}px`;
+        grid.style.width = `${canvasGrid-4}px`;
+        grid.classList.add = 'grid';
+        grid.className = 'grid';
+        row.appendChild(grid);
+        console.log(grid);
+        
+        
+    }
+    gridContainer.appendChild(row)
+}
