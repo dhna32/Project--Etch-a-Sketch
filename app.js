15027541462d13174713d86c5c249414a7feca92
Diegochits <54562062+dhna32@users.noreply.github.com>
2020-04-20 13:42:32 -0500

    Finished Etch-A-Sketch
    Finished first version of the project. Improvments soon

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const gridContainer = document.querySelector('#gridContainer');
-const gridSize = 720;
-const array = 64;
+const gridSize = 840;
 
-const canvasGrid = gridSize/array;
-console.log(canvasGrid);
-
-for (let i = 0; i < array; i++) {
-    const row = document.createElement('div');
-    row.classList.add = 'grid';
-    row.className = 'grid';
-    for (let j = 0; j < array; j++) {
-        const grid = document.createElement('div');
-        grid.style.height = `${canvasGrid-2}px`;
-        grid.style.width = `${canvasGrid-4}px`;
-        grid.classList.add = 'grid';
-        grid.className = 'grid';
-        row.appendChild(grid);
-        console.log(grid);
-        
-        
-    }
-    gridContainer.appendChild(row)
+function reset(){
+    gridContainer.innerHTML = '';
 }
+
+const gridButton = document.querySelector('#gridButton');
+
+gridButton.addEventListener('click', (e) => {
+    reset();
+    numberOfSquares = prompt("Select your grid size (1 to 64)");
+    let canvasGrid = gridSize/numberOfSquares;
+    for (let i = 0; i < numberOfSquares; i++) {
+        const row = document.createElement('div');
+        row.classList.add = 'grid';
+        row.className = 'grid';
+        for (let j = 0; j < numberOfSquares; j++) {
+            const grid = document.createElement('div');
+            grid.style.height = `${canvasGrid-2}px`;
+            grid.style.width = `${canvasGrid-5.01}px`;
+            grid.classList.add = 'grid';
+            grid.className = 'grid';
+            row.appendChild(grid);
+            console.log(grid);       
+        }
+        gridContainer.appendChild(row)
+    }  
+    gridContainer.addEventListener('mouseover', (e) => {
+        const gridValue = e.target;
+        console.log(gridValue);
+        if(gridValue){
+            gridValue.style.backgroundColor = 'black';
+        }
+    });
+});
+
